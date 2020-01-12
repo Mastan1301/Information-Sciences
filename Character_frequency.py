fb583211d43535b556df8e6b81d2bf3e3ac65dbf
Shaik Mastan <47546760+Mastan1301@users.noreply.github.com>
2020-01-12 19:49:03 +0530

    Update and rename EE18BTECH11039_HW1_problem1.6.py to Character_frequency.py

--- Character_frequency.py
+++ Character_frequency.py
@@ -1,5 +1,7 @@
 #Exercise 1.6
-#Use Python3 to run the program
+#Use Python 3 to run the program
+#   The program computes the frequencies of all the letters of the english alphabet
+#   in a given .txt file. In this program, the attached eng.txt is used.
 
 import matplotlib.pyplot as plt
 
@@ -9,14 +11,14 @@ def mapping(x): #returns the index value corresponding to the letter in the alph
     return a.index(x)
 
 f = open("eng.txt", 'r')
-arr = [0 for i in range(0, 26)] #list for storing the count of each letter
+p_arr = [0 for i in range(0, 26)] #list for storing the count of each letter
 total_count = 0
 for i in f.read():
     if ord('a') <= ord(i) <= ord('z'): #checking if the character is from the alphabet
-        arr[mapping(i)] += 1
+        p_arr[mapping(i)] += 1
         total_count += 1
 
-p_arr = [i/total_count for i in arr] #computing the probability
+p_arr = [i/total_count for i in p_arr] #computing the probability
 print(p_arr)
 
     
